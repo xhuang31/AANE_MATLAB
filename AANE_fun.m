ae0f3afe0a8432eeb9dbab7959825dc23e779629
xhuang31 <xhuang31@hawk.iit.edu>
2017-10-20 16:22:17 -0500

    Add files via upload

---
+++ AANE_fun.m
@@ -0,0 +1,126 @@
+function H = AANE_fun(Net,Attri,d,varargin)
+%Jointly embed Net and Attri into embedding representation H
+%     H = AANE_fun(Net,Attri,d);
+%     H = AANE_fun(Net,Attri,d,lambda,rho);
+%     H = AANE_fun(Net,Attri,d,lambda,rho,'Att');
+%     H = AANE_fun(Net,Attri,d,lambda,rho,'Att',worknum);
+% 
+%         Net   is the weighted adjacency matrix
+%        Attri  is the attribute information matrix with row denotes nodes
+%         d     is the dimension of the embedding representation
+%       lambda  is the regularization parameter
+%         rho   is the penalty parameter
+%        'Att'  refers to conduct Initialization from the SVD of Attri
+%       worknum is the number of worker used for distribution
+
+%   Copyright 2017, Xiao Huang and Jundong Li.
+%   $Revision: 1.0.0 $  $Date: 2017/10/18 00:00:00 $
+
+%% Parameters
+maxIter = 2; % Max num of iteration
+n = size(Net,1); % Total number of nodes
+Net(1:n+1:n^2) = 0;
+lambda = 0.1; % Initial regularization parameter
+rho = 5; % Initial penalty parameter
+Block = min(ceil(n/1),7575); % Treat each 7575 nodes as a block
+if ~isempty(varargin)
+    lambda = varargin{1};
+    rho = varargin{2};
+    if length(varargin) >= 3 && strcmp(varargin{3},'Att')
+        [~,MaxEdges] = sort(sum(Attri),'descend');
+        [H,~] = svds(Attri(:,MaxEdges(1:min(10*d,n))),d);
+    else
+        [~,MaxEdges] = sort(sum(Net),'descend');
+        [H,~] = svds(Net(:,MaxEdges(1:min(10*d,n))),d);
+    end
+    if length(varargin) >= 4
+        Block = min(ceil(n/varargin{4}),7575); % Treat each 7575 nodes as a block
+    end
+else
+    [~,MaxEdges] = sort(sum(Net),'descend');
+    [H,~] = svds(Net(:,MaxEdges(1:min(10*d,n))),d);
+end
+H = H'; % Transpose for speedup
+Z = H;
+Attri = Attri'; % Transpose for speedup
+Attri = bsxfun(@rdivide,Attri,sum(Attri.^2).^.5); % Normalization
+Attri(isnan(Attri)) = 0;
+%% First update H
+XTX = Z*Z'*2; % Transpose for speedup
+for Blocki = 1:ceil(n/Block) % Split nodes into different Blocks
+    IndexBlock = 1+Block*(Blocki-1); % First Index for splitting blcks
+    LocalIndex = IndexBlock:IndexBlock-1+min(n-IndexBlock+1,Block);
+    SA = Attri(:,LocalIndex)'*Attri; % Local affinity matrix for Blocki
+    sumS = Z*SA'*2;
+    for i = LocalIndex 
+        Neighbor = Z(:,Net(:,i)~=0); % the set of adjacent nodes of node i
+        for j = 1:1
+            normi_j = sum((bsxfun(@minus,Neighbor,H(:,i))).^2).^.5; % norm of h_i^k-z_j^k
+            nzIdx = logical(normi_j); % Non-equal Index
+            if any(nzIdx)
+                Wij = lambda*Net(i,Net(:,i)~=0);
+                normi_j = Wij(nzIdx)./normi_j(nzIdx);
+                H(:,i)=(XTX+(sum(normi_j)+rho)*eye(d))\(sumS(:,i-IndexBlock+1)+sum(bsxfun(@times,Neighbor(:,nzIdx),normi_j),2)+rho*Z(:,i));
+            else
+                H(:,i)=(XTX+rho*eye(d))\(sumS(:,i-IndexBlock+1)+rho*Z(:,i));
+            end
+        end
+    end
+end
+Affi = Blocki; % Index for affinity matrix
+U = zeros(d,n);
+%% Iterations
+for iter = 1:maxIter-1
+    %% Update Z
+    XTX = H*H'*2;
+    for Blocki = 1:ceil(n/Block) % Split nodes into different Blocks
+        IndexBlock = 1+Block*(Blocki-1); % Index for splitting blcks
+        LocalIndex = IndexBlock:IndexBlock-1+min(n-IndexBlock+1,Block);
+        if Affi ~= Blocki
+            SA = Attri(:,LocalIndex)'*Attri;
+            Affi = Blocki; % Index for affinity matrix
+        end
+        sumS = H*SA'*2;
+        for i = LocalIndex
+            Neighbor = H(:,Net(:,i)~=0); % the set of adjacent nodes of node i
+            for j = 1:1
+                normi_j = sum((bsxfun(@minus,Neighbor,Z(:,i))).^2).^.5;
+                nzIdx = logical(normi_j); % Non-equal Index
+                if any(nzIdx)
+                    Wij = lambda*Net(i,Net(:,i)~=0);
+                    normi_j = Wij(nzIdx)./normi_j(nzIdx);
+                    Z(:,i)=(XTX+(sum(normi_j)+rho)*eye(d))\(sumS(:,i-IndexBlock+1)+sum(bsxfun(@times,Neighbor(:,nzIdx),normi_j),2)+rho*(H(:,i)+U(:,i)));
+                else
+                    Z(:,i)=(XTX+rho*eye(d))\(sumS(:,i-IndexBlock+1)+rho*(H(:,i)+U(:,i)));
+                end
+            end
+        end
+    end
+    U=U+H-Z; % Update U
+    %% Update H
+    XTX = Z*Z'*2; % Transposed for speedup
+    for Blocki = 1:ceil(n/Block) % Split nodes into different Blocks
+        IndexBlock = 1+Block*(Blocki-1); % Index for splitting blcks
+        LocalIndex = IndexBlock:IndexBlock-1+min(n-IndexBlock+1,Block);
+        if Affi ~= Blocki
+            SA = Attri(:,LocalIndex)'*Attri;
+            Affi = Blocki; % Index for affinity matrix
+        end
+        sumS = Z*SA'*2;
+        for i = LocalIndex
+            Neighbor = Z(:,Net(:,i)~=0); % the set of adjacent nodes of node i
+            for j = 1:1
+                normi_j = sum((bsxfun(@minus,Neighbor,H(:,i))).^2).^.5;
+                nzIdx = logical(normi_j); % Non-equal Index
+                if any(nzIdx)
+                    Wij = lambda*Net(i,Net(:,i)~=0);
+                    normi_j = Wij(nzIdx)./normi_j(nzIdx);
+                    H(:,i)=(XTX+(sum(normi_j)+rho)*eye(d))\(sumS(:,i-IndexBlock+1)+sum(bsxfun(@times,Neighbor(:,nzIdx),normi_j),2)+rho*(Z(:,i)-U(:,i)));
+                else
+                    H(:,i)=(XTX+rho*eye(d))\(sumS(:,i-IndexBlock+1)+rho*(Z(:,i)-U(:,i)));
+                end
+            end
+        end
+    end
+end
+H=H';
---
+++ RunMe.m
@@ -0,0 +1,41 @@
+% Code for Accelerated Attributed Network Embedding
+% 
+%   Copyright 2017, Xiao Huang and Jundong Li.
+%   $Revision: 1.0.0 $  $Date: 2017/10/18 00:00:00 $
+
+%% Load data
+load('BlogCatalog.mat')
+lambda = 10^-0.6; % the regularization parameter
+rho = 5; % the penalty parameter
+% load('Flickr.mat')
+% lambda = 0.0425; % the regularization parameter
+% rho = 4; % the penalty parameter
+
+%% Experimental Settings
+d = 100; % the dimension of the embedding representation
+G = Network; % the weighted adjacency matrix
+A = Attributes; % the attribute information matrix with row denotes nodes
+clear Attributes & Network
+Indices = crossvalind('Kfold',length(G),25); % 5-fold cross-validation indices
+Group1 = find(Indices <= 10); % 2 for 10%, 5 for 25%, 10 for 50%, 20 for 100% of training group
+Group2 = find(Indices >= 21); % test group
+n1 = length(Group1); % num of nodes in training group
+n2 = length(Group2);  % num of nodes in test group
+CombG = sparse(G([Group1;Group2],[Group1;Group2]));
+CombA = sparse(A([Group1;Group2],:));
+
+
+%% Accelerated Attributed Network Embedding
+disp('Accelerated Attributed Network Embedding (AANE), 5-fold with 50% of training is used:')
+tic
+H = AANE_fun(sparse(CombG),sparse(CombA),d,lambda,rho);
+toc
+[F1macro1,F1micro1] = Performance(H(1:n1,:),H(n1+1:n1+n2,:),Label(Group1,:),Label(Group2,:)) %
+
+
+%% AANE for a Pure Network
+disp('AANE for a pure network:')
+tic
+H = AANE_fun(sparse(CombG),sparse(CombG),d,lambda,rho);
+toc
+[F1macro2,F1micro2] = Performance(H(1:n1,:),H(n1+1:n1+n2,:),Label(Group1,:),Label(Group2,:)) %
